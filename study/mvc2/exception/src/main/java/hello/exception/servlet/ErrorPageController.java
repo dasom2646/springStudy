cc5bffd8f619fa6b49a465b3a5cf96ba4d270afe
dasom2646 <choi.dasom2646@gmail.com>
2023-05-29 23:30:50 +0900

    [스프링 MVC 2편] 섹션9. API 예외 처리

--- study/mvc2/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ study/mvc2/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,18 @@
 package hello.exception.servlet;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -36,6 +43,22 @@ public class ErrorPageController {
 
     }
 
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+            HttpServletRequest request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+
+    }
+
+
     public void printErrorInfo(HttpServletRequest request) {
         log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
         log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
@@ -45,7 +68,7 @@ public class ErrorPageController {
         log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
 
 
-        log.info("dispatcherType={}",request.getDispatcherType());
+        log.info("dispatcherType={}", request.getDispatcherType());
 
     }
 }
